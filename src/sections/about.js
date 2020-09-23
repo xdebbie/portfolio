7e190613994129d9377a8c091dd2e9cc7d0a6914
xdebbie <deh@live.in>
2020-09-23 23:37:38 +0200

    About section again

--- src/sections/about.js
+++ src/sections/about.js
@@ -49,101 +49,166 @@ const About = () => {
 
           <ul className="lang">
             <div className="lang-box">
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Do it do it do it do it do it do it"
+                tabindex="0"
+              >
                 <img src={uk} className="flags" alt="uk" />
                 <img src={usa} className="flags" alt="usa" />
-                <span
-                  class="tooltip-toggle"
-                  aria-label="Sample text for your tooltip!"
-                  tabindex="0"
-                ></span>
                 <p>English</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={argentina} className="flags" alt="argentina" />
                 <img src={spain} className="flags" alt="spain" />
                 <p>Spanish</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="L'ocasió s'ha d'agafar pels pèls"
+                tabindex="0"
+              >
                 <img src={spain} className="flags" alt="spain" />
                 <p>Catalan</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Antes que eu me esqueça: fora Bolsonaro!"
+                tabindex="0"
+              >
                 <img src={portugal} className="flags" alt="portugal" />
                 <img src={brazil} className="flags" alt="brazil" />
                 <p>Portuguese</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Cest pas ma faulte"
+                tabindex="0"
+              >
                 <img src={france} className="flags" alt="france" />
                 <p>French</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Occidentali karma!"
+                tabindex="0"
+              >
                 <img src={italy} className="flags" alt="italy" />
                 <p>Italian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Ingen fara på taket, jag är här!"
+                tabindex="0"
+              >
                 <img src={sweden} className="flags" alt="sweden" />
                 <p>Swedish</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Du skød papegøjen, jeg er her! Ansæt mig nu"
+                tabindex="0"
+              >
                 <img src={denmark} className="flags" alt="denmark" />
                 <p>Danish</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Du kom ikke hele veien for å bare stoppe og snu seg rundt"
+                tabindex="0"
+              >
                 <img src={norway} className="flags" alt="norway" />
                 <p>Norwegian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={estonia} className="flags" alt="estonia" />
                 <p>Estonian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={finland} className="flags" alt="finland" />
                 <p>Finnish</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={romania} className="flags" alt="romania" />
                 <p>Romanian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={albania} className="flags" alt="albania" />
                 <p>Albanian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={russia} className="flags" alt="russia" />
                 <p>Russian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={ukraine} className="flags" alt="ukraine" />
                 <p>Ukrainian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={bulgaria} className="flags" alt="bulgaria" />
                 <p>Bulgarian</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={israel} className="flags" alt="israel" />
                 <p>Hebrew</p>
               </li>
-              <li>
+              <li
+                class="tooltip-toggle"
+                aria-label="Yo habia puesto una rata morida aqui... y ya no esta"
+                tabindex="0"
+              >
                 <img src={japan} className="flags" alt="japan" />
                 <p>Japanese</p>
               </li>
             </div>
           </ul>
-          <div className="education">
-            <h2>Bla bla bla education</h2>
-            <p>
-              All you need to know about my studies is probably on my{" "}
-              <a href="https://www.linkedin.com/in/deborah-caldeira/">
-                LinkedIn
-              </a>
-              , so come over and stalk me! And while you are at it, also add me
-              and send me a heya! If you are a former client, you are welcome to
-              write me a recommendation as well{" "}
-              <img src={pencil} className="text-icons" alt="pencil" />
-            </p>
-          </div>
+        </div>
+        <div className="education">
+          <h2>Bla bla bla education</h2>
+          <p>
+            All you need to know about my studies is probably on my{" "}
+            <a href="https://www.linkedin.com/in/deborah-caldeira/">LinkedIn</a>
+            , so come over and stalk me! And while you are at it, also add me
+            and send me a heya! If you are a former client, you are welcome to
+            write me a recommendation as well{" "}
+            <img src={pencil} className="text-icons" alt="pencil" />
+          </p>
         </div>
       </div>
     </div>
--- src/sections/stack.js
+++ src/sections/stack.js
@@ -10,14 +10,56 @@ const Stack = () => {
           <div className="title">
             <h1>/stack</h1>
           </div>
-          <ul>
-            <li>React</li>
-            <li>GraphQL</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Sass/SCSS</li>
-            <li>JavaScript</li>
-          </ul>
+          <div className="stack-boxes">
+            <div className="designer">
+              <p>Designer</p>
+              <ul>
+                <li>UX</li>
+                <li>UI</li>
+                <li>Web</li>
+                <li>Mobile</li>
+                <li>Apps</li>
+                <li>Logos</li>
+              </ul>
+              <p>Design tools</p>
+              <ul>
+                <li>Figma</li>
+                <li>Invision</li>
+                <li>Adobe XD</li>
+                <li>Adobe Illustrator</li>
+                <li>Adobe InDesign</li>
+                <li>Adobe Lightroom</li>
+                <li>Adobe Photoshop</li>
+                <li>Webflow</li>
+                <li>Zeplin</li>
+                <li>Marvel</li>
+                <li>Sketch</li>
+              </ul>
+            </div>
+            <div className="developer">
+              <p>Front end developer</p>
+              <ul>
+                <li>React</li>
+                <li>GraphQL</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Sass/SCSS</li>
+                <li>JavaScript</li>
+                <li>Gatsby</li>
+              </ul>
+              <p>Dev tools</p>
+              <ul>
+                <li>Atom</li>
+                <li>VSCode</li>
+                <li>Bootstrap</li>
+                <li>Codepen</li>
+                <li>Github</li>
+                <li>Gist</li>
+                <li>Terminal</li>
+                <li>Bash</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>

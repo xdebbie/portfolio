3651a66fe8be27aac30e91c48f5d59b69c560124
xdebbie <deh@live.in>
2021-11-04 21:15:06 +0100

    WIP: update contact-desktop-light

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "../sass/main.scss"
+import React from 'react'
+import '../sass/main.scss'
 
 // Import components
-import ContactContent from "../components/contactContent"
-import Map from "../components/map"
+import ContactContent from '../components/contactContent'
+// import Map from "../components/map"
 
 const Contact = () => (
     <div id="contact">
         <ContactContent />
-        <Map />
+        {/* <Map /> */}
     </div>
 )
 

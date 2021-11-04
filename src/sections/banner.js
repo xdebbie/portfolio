b44305b3322d093aef50c24affe5a4c23eceda01
xdebbie <deh@live.in>
2021-11-04 19:27:52 +0100

    WIP: update banner-desktop-light

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,40 +1,41 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+// import { useStaticQuery, graphql } from 'gatsby'
+// import Img from 'gatsby-image'
 import Aesthetic from '../assets/manicure.svg'
 import Pixelperfect from '../assets/pixel.svg'
 import Maintainability from '../assets/broom.svg'
 import Scalability from '../assets/lego.svg'
 import Responsiveness from '../assets/chameleon.svg'
+import Rigorous from '../assets/target.svg'
 
 const Banner = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            deb: file(relativePath: { eq: "deb1.jpg" }) {
-                childImageSharp {
-                    fluid(maxWidth: 300) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-        }
-    `)
+    // const data = useStaticQuery(graphql`
+    //     query {
+    //         deb: file(relativePath: { eq: "deb1.jpg" }) {
+    //             childImageSharp {
+    //                 fluid(maxWidth: 300) {
+    //                     ...GatsbyImageSharpFluid
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `)
 
     return (
         <div id="banner">
             <div className="container">
-                <div className="image-left">
+                {/* <div className="image-left">
                     <div className="profile-image">
                         <Img fluid={data.deb.childImageSharp.fluid} />
                     </div>
-                </div>
-                <div className="text-right">
+                </div> */}
+                <div className="hero">
                     <div className="title">
                         Ahoy! I'm <span>Deborah</span>.
                     </div>
                     <p>
-                        I am a <span>front-end web developer</span> based in
-                        Paris, France. I am currently working at{' '}
+                        I am a front-end web developer based in Paris, France. I
+                        am currently working at{' '}
                         <a
                             href="https://www.allowa.com"
                             target="_blank"
@@ -47,61 +48,87 @@ const Banner = () => {
                     </p>
                 </div>
                 <div className="attributes">
-                    <h2>
-                        This is what you will get with me... look no further!
-                    </h2>
+                    <p className="subtitle">
+                        this is what you will get with me... look no further!
+                    </p>
                     <ul>
                         <li>
-                            <Aesthetic className="att-icons" alt="aesthetic" />
-                            <p className="att-title">Aesthetic</p>
+                            <div>
+                                <Aesthetic
+                                    className="att-icons"
+                                    alt="aesthetic"
+                                />
+                                <p className="att-title">aesthetic</p>
+                            </div>
                             <p>
                                 I like it pretty, that is non-negotiable! And
                                 your customers will like it too
                             </p>
                         </li>
                         <li>
-                            <Pixelperfect
-                                className="att-icons"
-                                alt="pixel perfect"
-                            />
-                            <p>Pixel-perfect</p>
+                            <div>
+                                <Pixelperfect
+                                    className="att-icons"
+                                    alt="pixel perfect"
+                                />
+                                <p className="att-title">pixel-perfect</p>
+                            </div>
                             <p>
-                                My OCD will guarantee your designs are always
+                                my OCD will guarantee your designs are always
                                 presented faultlessly
                             </p>
                         </li>
                         <li>
-                            <Maintainability
-                                className="att-icons"
-                                alt="maintainability"
-                            />
-                            <p>Maintainability</p>
+                            <div>
+                                <Maintainability
+                                    className="att-icons"
+                                    alt="maintainability"
+                                />
+                                <p className="att-title">maintainability</p>
+                            </div>
                             <p>
-                                Easy to maintain, all my websites come with
+                                easy to maintain, all my websites come with
                                 extensive comments and instructions so you can
                                 keep it up and running for a long time
                             </p>
                         </li>
                         <li>
-                            <Scalability
-                                className="att-icons"
-                                alt="scalability"
-                            />
-                            <p>Scalability</p>
+                            <div>
+                                <Scalability
+                                    className="att-icons"
+                                    alt="scalability"
+                                />
+                                <p className="att-title">scalability</p>
+                            </div>
                             <p>
-                                Structured in a smart way, it is really simple
+                                structured in a smart way, it is really simple
                                 to just add a new page or section
                             </p>
                         </li>
                         <li>
-                            <Responsiveness
-                                className="att-icons"
-                                alt="responsiveness"
-                            />
-                            <p>Responsiveness</p>
+                            <div>
+                                <Responsiveness
+                                    className="att-icons"
+                                    alt="responsiveness"
+                                />
+                                <p className="att-title">responsiveness</p>
+                            </div>
                             <p>
-                                Mobile, tablet, desktop, super-large screens,
+                                mobile, tablet, desktop, super-large screens,
                                 dark mode? I've got your back
+                            </p>
+                        </li>
+                        <li>
+                            <div>
+                                <Rigorous
+                                    className="att-icons"
+                                    alt="rigorous testing"
+                                />
+                                <p className="att-title">rigorous testing</p>
+                            </div>
+                            <p>
+                                I ensure your website is of excellent quality by
+                                thoroughly testing using multiple approaches
                             </p>
                         </li>
                     </ul>
